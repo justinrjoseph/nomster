1f6c9723053640abe1abd74c0c7c442809146649
Justin Joseph <justin.joseph0@yahoo.com>
2015-11-14 16:47:51 +0000

    Add length validation to place name and presence validation to address and description

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ActiveRecord::Base
-  validates :name, presence: true
-  
+  validates :name, presence: true, length: { minimum: 4 }
+  validates :address, presence: true
+  validates :description, presence: true
+
   belongs_to :user
 end
