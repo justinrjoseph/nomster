75c45156c7e55e819fea1a665cdf368a46cc2cf5
Justin Joseph <justin.joseph0@yahoo.com>
2015-11-14 15:55:10 +0000

    Restrict deleting places to place creators

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def new
     @place = Place.new
@@ -38,7 +38,13 @@ class PlacesController < ApplicationController
   end
 
   def destroy
-    Place.find(params[:id]).destroy
+    @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+
+    @place.destroy
     redirect_to root_path
   end
 
