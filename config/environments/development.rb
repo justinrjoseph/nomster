0db92c25d4c80df078f585f0aac5e87ab4ca5677
Justin Joseph <justin.joseph0@yahoo.com>
2015-11-15 20:14:34 +0000

    Trigger comment added email after creation and disable emailtransmission in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,19 +12,19 @@ Nomster::Application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
+=begin
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'example.com',
-    authentication: 'plain',
+    authentication: :plain,
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_ADDRESS'],
     password: ENV['GMAIL_PASSWORD']
   }
-
+=end
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
 
   # Don't care if the mailer can't send.
