19093b91e7ca3a7b1e91f93f1876156dedc7486e
Justin Joseph <justin.joseph0@yahoo.com>
2015-11-15 03:58:47 +0000

    Add email preview in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,18 @@ Nomster::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'example.com',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
 
   # Don't care if the mailer can't send.
